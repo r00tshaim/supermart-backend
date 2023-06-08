32f0552c4c290fa08b697c6db875e2055f6249b9
Shaim Khanusiya <shaim456@gmail.com>
2023-06-08 11:57:30 +0530

    introduced http logs with morgan module and redirected morgan logs to wiston logs file

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ import productRoutes from "./routes/v1/productsRoutes.js";
 import categoriesRoutes from "./routes/v1/categoriesRoutes.js"
 import userRoutes from "./routes/v1/userRoutes.js";
 
+import morganMiddleware from "./middlewares/morgan.middleware.js";
+// The morgan middleware does not need this.
+// This is for a manual log
+import logger from "./logger/logger.js";
+
 dotenv.config();
 
 //connecting to db
 dbconnect();
 
 const app = express();
+
+// Add the morgan middleware
+app.use(morganMiddleware);
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -25,7 +33,11 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/categories", categoriesRoutes);
 
 app.get("/test", (req, res) => {
-  res.send("REST API server is running");
+  logger.info("Checking the API status: Everything is OK")
+  res.status(200).send({
+    status: "UP",
+    message: "The API is up and running!"
+  });
 });
 
 
@@ -34,6 +46,7 @@ const PORT = process.env.PORT || 5555;
 
 app.listen(
   PORT,
+  logger.info(`Server running in ${process.env.NODE_ENV || "production" } mode on port ${PORT}`),
   console.log(
     `Server running in ${process.env.NODE_ENV || "production" } mode on port ${PORT}`
   )
