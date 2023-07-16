2e7e93c6e91c6189fcb239b139f7b1226f3861ef
Shaim Khanusiya <shaim456@gmail.com>
2023-07-16 17:23:57 +0530

    fix process crash when used email or mobile is entered

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -4,8 +4,8 @@ import logger from "../logger/logger.js";
 function ErrorHandlerMiddleware(err, req, res, next) {
   // in prod, don't use console.log or console.err because
   // it is not async
-  logger.error("err=",err)
-  console.error("err=",err);
+  //logger.error("err=",err)
+  //console.error("err=",err);
 
   if (err instanceof ErrorHandler) {
     logger.info("err is instance of ErrorHandler")
